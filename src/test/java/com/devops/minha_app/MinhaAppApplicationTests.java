76c53803f6e41b94882fefb0ed404f35dcb00236
Andressa Romualdo <andressaromualdocosta@gmail.com>
2026-06-27 09:53:45 -0300

    fix: ativa profile test para desabilitar kafka nos testes

--- src/test/java/com/devops/minha_app/MinhaAppApplicationTests.java
+++ src/test/java/com/devops/minha_app/MinhaAppApplicationTests.java
@@ -2,12 +2,14 @@ package com.devops.minha_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MinhaAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
